24961f0b06937e76724802172b2b90a239347846
Adam <aggiacca@umich.edu>
2018-03-27 20:20:50 -0400

    closure progress

--- main.py
+++ main.py
@@ -4,6 +4,25 @@ def printList(list):
         print(item)
 
 
+class Rule:
+    def __init__(self, nonterminal, dependency):
+        self.lhs = nonterminal
+        self.rhs = dependency
+
+
+def closure(targetNT, rules):
+    # first take any rules where targetChar is on the left hand side
+    #   then add dot to beginning of rhs
+
+
+    # check subclosure
+    # option 1 to recursivly take closure of chars to the right of dots
+    # option 2 use stack to avoid duplicates
+
+    # create stack with nonterminals to check
+
+
+
 if __name__ == "__main__":
 
     file = open("example-input", "r")
@@ -12,3 +31,4 @@ if __name__ == "__main__":
 
     printList(terminals)
     printList(nonterminals)
+
